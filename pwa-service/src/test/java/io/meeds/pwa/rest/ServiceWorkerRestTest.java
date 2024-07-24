7fc04943e955d63ce91db254ae166b684709503a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-07-24 13:39:40 +0100

    feat: Add Notification choice in User Settings - MEED-7309 - Meeds-io/MIPs#134 (#4)

---
+++ pwa-service/src/test/java/io/meeds/pwa/rest/ServiceWorkerRestTest.java
@@ -0,0 +1,88 @@
+package io.meeds.pwa.rest;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import io.meeds.pwa.service.PwaSwService;
+import io.meeds.spring.web.security.PortalAuthenticationManager;
+import io.meeds.spring.web.security.WebSecurityConfiguration;
+
+import jakarta.servlet.Filter;
+
+@SpringBootTest(classes = { ServiceWorkerRest.class, PortalAuthenticationManager.class, })
+@ContextConfiguration(classes = { WebSecurityConfiguration.class })
+@AutoConfigureWebMvc
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class ServiceWorkerRestTest {
+
+  private static final String REST_PATH = "/service-worker"; // NOSONAR
+
+  static final ObjectMapper   OBJECT_MAPPER;
+
+  static {
+    // Workaround when Jackson is defined in shared library with different
+    // version and without artifact jackson-datatype-jsr310
+    OBJECT_MAPPER = JsonMapper.builder()
+                              .configure(JsonReadFeature.ALLOW_MISSING_VALUES, true)
+                              .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                              .build();
+    OBJECT_MAPPER.registerModule(new JavaTimeModule());
+  }
+
+  @MockBean
+  private PwaSwService          pwaSwService;
+
+  @Autowired
+  private SecurityFilterChain   filterChain;
+
+  @Autowired
+  private WebApplicationContext context;
+
+  private MockMvc               mockMvc;
+
+  @BeforeEach
+  void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                             .addFilters(filterChain.getFilters().toArray(new Filter[0]))
+                             .build();
+  }
+
+  @Test
+  void getNotFound() throws Exception {
+    ResultActions response = mockMvc.perform(get(REST_PATH));
+    response.andExpect(status().isNotFound());
+  }
+
+  @Test
+  void getContent() throws Exception {
+    when(pwaSwService.getContent()).thenReturn("content");
+    ResultActions response = mockMvc.perform(get(REST_PATH));
+    response.andExpect(status().isOk()).andExpect(header().stringValues("Service-Worker-Allowed", "/"));
+  }
+
+}
